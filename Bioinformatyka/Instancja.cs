0bf85aaff0d651533af48473f26811352b16f8ef
Kamil <Kamil@DESKTOP-S39RJG7>
2019-04-13 23:18:25 +0200

    Dodaj pliki projektów.

---
+++ Bioinformatyka/Instancja.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Bioinformatyka
+{
+    class Instancja
+    {
+        public HashSet<string> Spectrum;
+        private string Sekwencja;
+        public Instancja(string FileName, int Dlugosc)
+        {
+            StreamReader reader = new StreamReader(FileName);
+            this.Sekwencja = reader.ReadLine();
+            if(!this.Sekwencja.All((character) =>
+            {
+                if (character != 'A' && character != 'C' && character != 'T' && character != 'G')
+                {
+                    return false;
+                }
+                return true;
+            }))
+            {
+                throw new Exception("Wykryto nieprawidłowe elementy sekwencji");
+            }
+            for(int i = 0; i < this.Sekwencja.Length - Dlugosc; i++)
+            {
+                Spectrum.Add(this.Sekwencja.Substring(i, Dlugosc)); 
+            }
+
+        }
+        public override string ToString()
+        {
+            return String.Join("\n", this.Sekwencja);
+        }
+    }
+}
